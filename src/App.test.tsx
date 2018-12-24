08481ccad9730097cd19b86cab301414ce0af6f1
AlexanderLapygin <AlexanderLapygin@gmail.com>
2018-12-24 15:49:15 +0300

    refactor: formatting

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,15 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import App from "./App";
 import { shallow } from 'enzyme';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from './App';
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
+it('renders without crashing', () => {
+  const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
-it("renders the heading", () => {
+it('renders the heading', () => {
   const wrapper = shallow(<App />);
-  expect(wrapper.find("h1").text()).toBe("Hello React");
+  expect(wrapper.find('h1').text()).toBe('Hello React');
 });
